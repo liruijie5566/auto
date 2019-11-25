185a03979d8c79d15f63e3812401491d4cd3b9b6
jerry <liruijie5566@163.com>
2019-11-25 13:54:54 +0800

    Add files via upload

--- 快手极速版.js
+++ 快手极速版.js
@@ -4,9 +4,9 @@ util.checkOpen("快手极速版","1");
 sleep(8000);
 util.checkapp(getPackageName("快手极速版"));
 //util.Click(0.3,0.4);
-sleep(random(25000,40000));
+sleep(random(20000,30000));
 while(true){
     util.swipeall(0.7,0.2);
-    sleep(random(25000,40000));
+    sleep(random(20000,30000));
 	util.checkapp(getPackageName("快手极速版"));
 }
